b8a9c1f33e419c8ebd59d57c43730b82d2d2b651
Muiduzzaman Mahim <47756767+muiduzzaman-mahim@users.noreply.github.com>
2021-02-19 08:58:38 +0600

    updated

---
+++ js/hide_show.js
@@ -0,0 +1,95 @@
+// topbar 
+function toplink() {
+    document.getElementById("top").style.display = "block"
+    document.getElementById("about").style.display = "none"
+    document.getElementById("skills").style.display = "none"
+    document.getElementById("project").style.display = "none"
+    document.getElementById("experience").style.display = "none"
+    document.getElementById("study").style.display = "none"
+    document.getElementById("resume").style.display = "none"
+    document.getElementById("body1").style.backgroundImage = "url('../images/Logo12.jpg')"
+}
+
+// about section 
+function aboutlink() {
+    document.getElementById("top").style.display = "none"
+    document.getElementById("about").style.display = "block"
+    document.getElementById("skills").style.display = "none"
+    document.getElementById("project").style.display = "none"
+    document.getElementById("experience").style.display = "none"
+    document.getElementById("study").style.display = "none"
+    document.getElementById("resume").style.display = "none"
+    document.getElementById("body1").style.backgroundImage = "url('../images/logo12.jpg')"
+}
+
+// projects list with link 
+function project() {
+    document.getElementById("top").style.display = "none"
+    document.getElementById("about").style.display = "none"
+    document.getElementById("skills").style.display = "none"
+    document.getElementById("project").style.display = "block"
+    document.getElementById("experience").style.display = "none"
+    document.getElementById("study").style.display = "none"
+    document.getElementById("resume").style.display = "none"
+    document.getElementById("body1").style.backgroundImage = "none"
+}
+
+// skills highlight 
+function skills() {
+    document.getElementById("top").style.display = "none"
+    document.getElementById("about").style.display = "none"
+    document.getElementById("skills").style.display = "block"
+    document.getElementById("project").style.display = "none"
+    document.getElementById("experience").style.display = "none"
+    document.getElementById("study").style.display = "none"
+    document.getElementById("resume").style.display = "none"
+    document.getElementById("body1").style.backgroundImage = "url('../images/logo13.jpg')"
+}
+
+// experiences 
+function exp() {
+    document.getElementById("top").style.display = "none"
+    document.getElementById("about").style.display = "none"
+    document.getElementById("skills").style.display = "none"
+    document.getElementById("project").style.display = "none"
+    document.getElementById("experience").style.display = "block"
+    document.getElementById("study").style.display = "none"
+    document.getElementById("resume").style.display = "none"
+    document.getElementById("body1").style.backgroundImage = "url('../images/logo13.jpg')"
+}
+
+// educational background 
+function study() {
+    document.getElementById("top").style.display = "none"
+    document.getElementById("about").style.display = "none"
+    document.getElementById("skills").style.display = "none"
+    document.getElementById("project").style.display = "none"
+    document.getElementById("experience").style.display = "none"
+    document.getElementById("study").style.display = "block"
+    document.getElementById("resume").style.display = "none"
+    document.getElementById("body1").style.backgroundImage = "url('../images/logo13.jpg')"
+}
+
+// contact with me 
+function contact() {
+    document.getElementById("top").style.display = "none"
+    document.getElementById("about").style.display = "none"
+    document.getElementById("skills").style.display = "none"
+    document.getElementById("project").style.display = "none"
+    document.getElementById("experience").style.display = "none"
+    document.getElementById("study").style.display = "none"
+    document.getElementById("resume").style.display = "none"
+    document.getElementById("body1").style.backgroundImage = "none"
+}
+
+// resume 
+function resume() {
+    document.getElementById("top").style.display = "none"
+    document.getElementById("about").style.display = "none"
+    document.getElementById("skills").style.display = "none"
+    document.getElementById("project").style.display = "none"
+    document.getElementById("experience").style.display = "none"
+    document.getElementById("study").style.display = "none"
+    document.getElementById("resume").style.display = "block"
+    document.getElementById("body1").style.backgroundImage = "url('../images/logo13.jpg')"
+}
